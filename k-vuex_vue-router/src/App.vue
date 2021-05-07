96c0ffe6bcc2c918214991436ef6e95808aa178e
kongjun0320 <1741142595@qq.com>
2021-05-07 21:48:54 +0800

    去掉console

--- k-vuex_vue-router/src/App.vue
+++ k-vuex_vue-router/src/App.vue
@@ -1,16 +1,9 @@
 <template>
   <div id="app">
-    <!-- vuex demo - zf -->
-    <!-- <p>珠峰的年龄: {{ $store.state.age }}</p>
-    <p>我的年龄: {{ $store.getters.myAge }}</p>
-    <button @click="syncChange">同步更改</button>
-    <button @click="asyncChange">异步更改</button> -->
-    <!-- vuex demo - kkb -->
     <!-- <p @click="$store.commit('add')">sync {{ $store.state.counter }}</p>
     <p @click="$store.dispatch('add')">async {{ $store.state.counter }}</p>
     <p>doubleCounter: {{ $store.getters.doubleCounter }}</p>
     <p>tripleCounter: {{ $store.getters.tripleCounter }}</p> -->
-    <!-- vue-router demo -->
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -20,15 +13,7 @@
 </template>
 <script>
 export default {
-  name: 'App',
-  methods: {
-    syncChange() {
-      this.$store.commit('syncChange', 10)
-    },
-    asyncChange() {
-      this.$store.dispatch('asyncChange', 5)
-    }
-  }
+  name: 'App'
 }
 </script>
 
